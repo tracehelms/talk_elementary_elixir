1a96b3709ebdf12a3e7bdd35dc8db466998e3f3f
tracehelms <tracehelms@gmail.com>
2015-12-07 09:27:53 -0700

    updated progress

---
+++ code/processes.exs
@@ -0,0 +1,26 @@
+defmodule Processes do
+  def counter(next_pid) do
+    receive do
+      n ->
+        send(next_pid, n+1)
+    end
+  end
+
+  def create_processes(n) do
+    last = Enum.reduce(1..n, self, fn(_, send_to) ->
+      spawn(Processes, :counter, [send_to])
+    end)
+
+    send last, 0
+
+    receive do
+      final_answer when is_integer(final_answer) ->
+        "Result is #{inspect(final_answer)}"
+    end
+  end
+
+  def run(n) do
+    IO.puts inspect :timer.tc(Processes, :create_processes, [n])
+  end
+
+end
